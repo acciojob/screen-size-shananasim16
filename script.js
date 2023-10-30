b7373fd2ae20cbbca98a89958a4c70a6460181bd
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-30 19:02:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-function updateWindowSize() {
-  const widthElement = document.getElementById("width");
-  const heightElement = document.getElementById("height");
-
-  // Get the window's width and height
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-
-  // Update the text content of the <span> elements
-  widthElement.textContent = width;
-  heightElement.textContent = height;
-}
-
-// Initial update on page load
-updateWindowSize();
-
-// Attach the updateWindowSize function to the window resize event
-window.addEventListener("resize", updateWindowSize);
+function getSize() {
+			var w = document.documentElement.clientWidth;
+			var h = document.documentElement.clientHeight;
+			document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
+		}
 
